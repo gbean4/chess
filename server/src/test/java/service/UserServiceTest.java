5cf62ae20be5bd1319b040d9fd1486da8fb11e40
gbean4 <gbean4@byu.edu>
2025-10-21 20:54:00 -0600

    register, login Service tests

--- server/src/test/java/service/UserServiceTest.java
+++ server/src/test/java/service/UserServiceTest.java
@@ -1,6 +1,5 @@
 package service;
 
-import dataaccess.DataAccess;
 import dataaccess.MemoryDataAccess;
 import datamodel.RegisterResponse;
 import datamodel.UserData;
@@ -11,7 +10,7 @@ import static org.junit.jupiter.api.Assertions.*;
 class UserServiceTest {
 
     @Test
-    void register() throws Exception {
+    void registerPositive() throws Exception{
         var db = new MemoryDataAccess();
         var service = new UserService(db);
         var user = new UserData("lee", "2@c","password");
@@ -21,26 +20,35 @@ class UserServiceTest {
         assertNotNull(response);
         assertEquals("lee", response.username());
         assertNotNull(response.authToken(), "authToken should not be null");
-        var storedAuth = db.getAuth(response.authToken());
-        assertNotNull(storedAuth, "oh man it's not stored in db");
-        assertEquals("lee", storedAuth.username());
-        System.out.println(storedAuth.authToken().equals(response.authToken()));
     }
+
+
+
     @Test
-    void login() throws Exception{
+    void registerNegative() throws Exception {
+        var db = new MemoryDataAccess();
+        var service = new UserService(db);
+        var user = new UserData("lee", "2@c","password");
+
+        RegisterResponse response = service.register(user);
+
+        Exception ex = assertThrows(Exception.class, () -> service.register(user));
+        assertTrue(ex.getMessage().contains("exists"));
+    }
+
+    @Test
+    void loginPositive() throws Exception{
         var db = new MemoryDataAccess();
         var service = new UserService(db);
         var user = new UserData("lee", "2@c","password");
 
         service.register(user);
 
-        var firstAuth = service.login("lee", "password");
-        assertNotNull(firstAuth);
-        assertNotNull(firstAuth.authToken());
-
-        var secondAuth = service.login("lee", "password");
-        assertNotEquals(firstAuth.authToken(), secondAuth.authToken(), "authTokens should be different");
-    }
+        var auth = service.login("lee", "password");
+        assertNotNull(auth);
+        assertEquals("lee", auth.username());
+        assertNotNull(db.getAuth(auth.authToken()));
+     }
 
     @Test
     void logout() throws Exception{
