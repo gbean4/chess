f09e887e7013f2da70e34c67ad7f7db130e65919
gbean4 <gbean4@byu.edu>
2025-11-08 18:07:18 -0700

    Tentative ChessboardUI setup

--- client/src/main/java/ui/ChessBoardUI.java
+++ client/src/main/java/ui/ChessBoardUI.java
@@ -10,24 +10,35 @@ import static ui.EscapeSequences.*;
 
 public class ChessBoardUI {
 
-    public static void render(ChessGame game){
+    public static void renderBoard(ChessGame game){
         ChessBoard board = game.getBoard();
 
-        System.out.println("    a  b  c  d  e  f  g  h");
-        System.out.print(SET_BG_COLOR_LIGHT_GREY + "8" + RESET_BG_COLOR);
+        System.out.print(SET_BG_COLOR_LIGHT_GREY + "    a  b  c  d  e  f  g  h" + RESET_BG_COLOR);
 
-        for (int row = 8; row>=1; row --){
-            System.out.print(row + " | ");
-            for (int col = 1; col<= 8; col++){
+        for (int row = 0; row<8; row ++) {
+            int rank = 8 - row;
+            System.out.print(SET_BG_COLOR_LIGHT_GREY + " " + rank + " " + RESET_BG_COLOR);
+
+            String bg = null;
+            String color = null;
+            String symbol = null;
+            for (int col = 1; col <= 8; col++) {
+                boolean isDark = (row + col) % 2 == 0;
+                bg = isDark ? SET_BG_COLOR_GREEN : SET_BG_COLOR_DARK_GREEN;
+
                 ChessPosition pos = new ChessPosition(row, col);
                 ChessPiece piece = board.getPiece(pos);
-                String symbol = getSymbol(piece);
-                System.out.print(symbol + " ");
+
+                if (piece != null) {
+                    symbol = getSymbol(piece);
+                    color = (piece.getTeamColor() == ChessGame.TeamColor.WHITE ?
+                            SET_TEXT_COLOR_WHITE : SET_TEXT_COLOR_BLACK);
+                }
+                System.out.print(bg + color + " " + symbol + " " + RESET_BG_COLOR+ RESET_TEXT_COLOR);
             }
-            System.out.println("| " + row);
+            System.out.println(SET_BG_COLOR_LIGHT_GREY + " " + rank + " " + RESET_BG_COLOR);
         }
-        System.out.println("  --------------------------");
-        System.out.println("    a  b  c  d  e  f  g  h");
+        System.out.println(SET_BG_COLOR_LIGHT_GREY + "    a  b  c  d  e  f  g  h" + RESET_BG_COLOR);
     }
 
     private static String getSymbol(ChessPiece piece){
